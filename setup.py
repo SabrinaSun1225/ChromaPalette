065355c17239cb7b0df635a487b6d4c319a9ec81
SabrinaSun1225 <129010435+SabrinaSun1225@users.noreply.github.com>
2023-04-16 16:08:38 -0400

    Update setup.py
    new version, new colors

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ChromaPalette',
-    version='0.1.0',
+    version='0.1.1',
     description='A Versatile Scientific Color Palette Library',
 
     author='Sabrina Sun',
